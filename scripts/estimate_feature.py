bf2a989336721081d340730078d7d598a4da1ddf
Tatsuro Sakaguchi <tatsuro.sakaguchi@g.softbank.co.jp>
2024-01-18 11:03:21 +0900

    Add option to remove background

--- scripts/estimate_feature.py
+++ scripts/estimate_feature.py
@@ -24,6 +24,7 @@ import numpy as np
 import rospy
 from box_mot.msg import Features
 from jsk_recognition_msgs.msg import ClassificationResult
+from jsk_recognition_msgs.msg import ClusterPointIndices
 from jsk_recognition_msgs.msg import RectArray
 from sensor_msgs.msg import Image
 from std_msgs.msg import Float32MultiArray
@@ -35,22 +36,38 @@ class EstimateFeature(object):
 
     def __init__(self) -> None:
         model_path = pathlib.Path(rospy.get_param('~model', 'osnet_x0_25_msmt17.pt'))
+        self.__remove_background = rospy.get_param('~remove_background', False)
         self.__model = ReIDDetectMultiBackend(weights=model_path, device='cuda', fp16=True)
         self.__pub = rospy.Publisher('features', Features, queue_size=1)
         image_sub = message_filters.Subscriber('image', Image)
         rects_sub = message_filters.Subscriber('rects', RectArray)
         results_sub = message_filters.Subscriber('results', ClassificationResult)
-        self.__sync = message_filters.TimeSynchronizer([image_sub, rects_sub, results_sub], 10)
+        if self.__remove_background:
+            indices_sub = message_filters.Subscriber('indices', ClusterPointIndices)
+            self.__sync = message_filters.TimeSynchronizer([image_sub, rects_sub, results_sub, indices_sub], 10)
+        else:
+            self.__sync = message_filters.TimeSynchronizer([image_sub, rects_sub, results_sub], 10)
         self.__sync.registerCallback(self.__callback)
         self.__bridge = cv_bridge.CvBridge()
 
-    def __callback(self, img: Image, rects: RectArray, results: ClassificationResult) -> None:
+    def __callback(self,
+                   img: Image,
+                   rects: RectArray,
+                   results: ClassificationResult,
+                   indices: ClusterPointIndices = None) -> None:
+        null_image = np.zeros((img.height * img.width, 3), dtype=np.uint8)
         image = self.__bridge.imgmsg_to_cv2(img, desired_encoding='bgr8')
+        base_image = image.reshape(-1, 3)
         detections = []
-        for rect, name, prob, label_id in zip(rects.rects, results.label_names, results.label_proba, results.labels):
-            # if name != 'person':
-            #     continue
-            detections.append([rect.x, rect.y, rect.x + rect.width, rect.y + rect.height, prob, label_id])
+        if indices is None:
+            for rect, prob, label_id in zip(rects.rects, results.label_proba, results.labels):
+                detections.append([rect.x, rect.y, rect.x + rect.width, rect.y + rect.height, prob, label_id])
+        else:
+            for rect, prob, indice, label_id in zip(rects.rects, results.label_proba, indices.cluster_indices,
+                                                    results.labels):
+                detections.append([rect.x, rect.y, rect.x + rect.width, rect.y + rect.height, prob, label_id])
+                null_image[indice.indices, :] = base_image[indice.indices, :]
+            image = null_image.reshape(img.height, img.width, 3)
         if not detections:
             return
         detections = np.array(detections)
